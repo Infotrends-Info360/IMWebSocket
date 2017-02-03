54d35bb595b3d0c6967da69a9254afce914f5fd4
SamTsai <uopsdod@gmail.com>
2017-02-03 13:54:37 +0800

    清除多餘的entrylog寫入 client.js 去掉JACK寫的ajax程式碼

--- WebContent/js/websocket-client.js
+++ WebContent/js/websocket-client.js
@@ -202,46 +202,6 @@ function Login() {
 
 					isonline = true;
 					
-					//Jack
-					var LoginTime = document.getElementById("Login_time").value;
-					var browserversion = document.getElementById("Browser_Version").value;
-					var Channeltype = document.getElementById("Channeltype").value;
-					var language = document.getElementById("web_language").value;
-					var platfrom = document.getElementById("Platfrom").value;
-					var Cli_IP = document.getElementById("Cli_IP").value;
-					
-					/** ServiceEntry console log **/
-				     $.ajax({
-				         //告訴程式表單要傳送到哪裡                                         
-				         url:"/IMWebSocket/RESTful/ServiceEntry",
-				         //需要傳送的資料
-				         data:{
-				        	 username:UserName,
-				        	 userid:UserID,
-				        	 browserversion:browserversion,
-				        	 language:language,
-				        	 entertime:LoginTime,
-				        	 channeltype:Channeltype,
-				        	 platfrom:platfrom,
-				        	 ipaddress:Cli_IP
-				        	 },
-				          //使用POST方法     
-				         type : "POST",                                                                    
-				         //接收回傳資料的格式，在這個例子中，只要是接收true就可以了
-				         dataType:'json', 
-				          //傳送失敗則跳出失敗訊息          
-				         error:function(e){                                                                 
-				         alert("失敗");
-				         callback(data);
-				         },
-				         success:function(data){                                                           
-				        	 console.log("login",data)
-			    
-				         },
-				      
-				     }); 
-				   //Jack	
-					
 				}  else if ("responseThirdParty" == obj.Event){
 					document.getElementById("currRoomID").innerHTML = obj.roomID;
 					document.getElementById("currUsers").innerHTML = obj.roomMembers;
