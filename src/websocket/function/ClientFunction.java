1a5fc5f3783010dd5586a9204cb9c5cfa1ca887c
uopsdod <uopsdod@gmail.com>
2017-03-03 16:49:06 +0800

    還原為開發版本

--- src/websocket/function/ClientFunction.java
+++ src/websocket/function/ClientFunction.java
@@ -67,16 +67,16 @@ public class ClientFunction {
 			senduserdataObj.addProperty("sendto", AgentID); // 重點
 			senduserdataObj.addProperty("searchtype", "A");
 			JsonObject attributes = new JsonObject();
-			// 上192.168.10.42版本:
-//			attributes.addProperty("attributenames", "Phone,id,service1,service2");
-//			attributes.addProperty("Phone",userName); // 特別注意一下名稱並沒對到
-//			attributes.addProperty("id",userID);
-//			attributes.addProperty("service1","service one");
-//			attributes.addProperty("service2","service two");
-			// 正式版本:
-			attributes.addProperty("attributenames", "IDNO_");
-			attributes.addProperty("IDNO_",userName);
+			// localhost, 192.168.10.42版本:
+			attributes.addProperty("attributenames", "Phone,id,service1,service2");
+			attributes.addProperty("Phone",userName); // 特別注意一下名稱並沒對到
 			attributes.addProperty("id",userID);
+			attributes.addProperty("service1","service one");
+			attributes.addProperty("service2","service two");
+			// 客戶端版本:
+//			attributes.addProperty("attributenames", "IDNO_");
+//			attributes.addProperty("IDNO_",userName);
+//			attributes.addProperty("id",userID);
 
 			senduserdataObj.add("attributes", attributes);
 			senduserdataObj.addProperty("channel", "chat");
